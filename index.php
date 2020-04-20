6f7c1e5f1253dc855edab4228dd295f87d910ce3
Rad Geek <feedback+finarfin@radgeek.com>
2020-04-20 12:58:38 +0000

    covid-data dir, ArchivedSource, index.php / allow for finding HTML source and related capture files when placed into a timestamped subdirectory.

--- index.php
+++ index.php
@@ -177,18 +177,31 @@ function get_slug_timestamps ($what, $where) {
 function get_snapshot_lists ($dir) {
 	global $params;
 
-	$files = array_merge(
-		glob("${dir}/*.url.txt"),
-		glob("${dir}/data/*.url.txt"),
-		glob("${dir}/html/*.url.txt")
-	);
+	$subDirs = [ "${dir}/data", "${dir}/html" ];
+	$urlDirectories = $subDirs;
+	foreach ($urlDirectories as $subDir) :
+		$subSubDirs = glob("${subDir}/202[0-9]*Z", GLOB_ONLYDIR);
+		$urlDirectories = array_merge($urlDirectories, $subSubDirs);
+	endforeach;
+
+	$files = [];
+	foreach ($urlDirectories as $urlDir) :
+		$files = array_merge($files, glob("${urlDir}/*.url.txt"));
+	endforeach;
 	
 	$pairs = []; $allSlugs = [];
 	foreach ($files as $file) :
 		$basedir = preg_replace("|^".preg_quote($dir)."|i", "", dirname($file));
+		
 		$base = basename($file);
 		if (preg_match("|^([^-]+)(-([0-9]+Z))?[.]url[.]txt$|i", $base, $m)) :
-			$set = [(strlen($basedir) > 0 ? "${basedir}/" : "") . $m[1], $m[3], $basedir];
+			$slug = $m[1];
+			$ts = $m[3];
+			if (basename($basedir)==$ts) :
+				$basedir = dirname($basedir);
+			endif;
+			
+			$set = [(strlen($basedir) > 0 ? "${basedir}/" : "") . $slug, $ts, $basedir];
 			$allSlugs[] = $set;
 			if (is_null($params['slug']) or ($set[0]==$params['slug'])) :
 				$pairs[] = $set;
